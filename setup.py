838a2e83ee39d41e16610d50f24a295e3c7dd225
Supratim Dey Choudhury <supratimdc123@gmail.com>
2025-04-19 17:46:39 +0530

    Updating setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
-    name='finwhiz',
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+    
+REPO_NAME = "finwhizplus"
+AUTHOR_USER_NAME = "supratimdc123"
+SRC_REPO = "finwhizplus"
+AUTHOR_EMAIL = "supratimdc123@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
     version='0.1',
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        'flask', 'pandas', 'scikit-learn', 'yfinance',
-        'nltk', 'transformers', 'torch', 'joblib', 'matplotlib', 'seaborn',
-        'tweepy', 'requests'
-    ],
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A Python library for financial data analysis and visualization.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
 )
